5d63232180dbbedc348f65eced152a5848182adc
Guilherme <guimoltri@gmail.com>
2022-09-11 21:57:05 -0300

    Tentando fazer o deploy novamente

--- storage/framework/views/32e0dfbf61a832ef7c7646e62120faf56bc85dac.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\Users\salda\OneDrive - ifsp.edu.br\Área de Trabalho\FastSolutions\Desenvolvimento\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/subcopy.blade.php ENDPATH**/ ?>
